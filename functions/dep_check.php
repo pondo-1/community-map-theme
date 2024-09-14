83b4560b8149f5ae3f5f2c3efc856c707ff4e1af
Diane Kang <diane.kang@page-effect.com>
2024-09-14 21:36:03 +0200

    minor change

--- functions/dep_check.php
+++ functions/dep_check.php
@@ -11,7 +11,6 @@ function check_required_plugin()
   if (!is_plugin_active($required_plugin)) {
     // Display a warning message in the admin area
     add_action('admin_notices', 'show_plugin_warning');
-    add_filter('acf/settings/save_json', 'my_acf_json_save_point');
   }
 }
 
@@ -21,9 +20,4 @@ function show_plugin_warning()
   echo '<div class="notice notice-error is-dismissible">
            <p><strong>Warning:</strong> This theme requires plugin <em>advanced custom fields pro</em>. But that is not installed or activated. Please install and activate the plugin to ensure proper functionality of the theme.</p>
        </div>';
-}
-
-function my_acf_json_save_point($path)
-{
-  return get_stylesheet_directory() . '/test';
 }
