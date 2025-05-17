3fbcc80f659fe1d8b3e2e52113598c1d2e2bf72e
Diane Kang <diane.kang@page-effect.com>
2025-05-17 18:03:55 +0200

    add logo setting on the map

--- template-parts/nav-top.php
+++ template-parts/nav-top.php
@@ -1,7 +1,17 @@
 <ul class="menu top">
   <li>
+    <!--  if the options of get_field('Title_option', 'option') == text or == logo -->
+
     <a class="logo" href="/">
-      <h1 class="logo"><span><?php echo bloginfo('title'); ?></span></h1>
+      <?php
+      $title_option = get_field('title_option', 'option');
+      if ($title_option == 'text') {
+        echo '<h1 class="logo"><span>' . bloginfo('title') . '</span></h1>';
+      } elseif ($title_option == 'logo') {
+        $logo = get_field('logo', 'option');
+        echo '<img src="' . esc_url($logo['url']) . '" alt="' . esc_attr($logo['alt']) . '" />';
+      }
+      ?>
     </a>
   </li>
   <?php $welcome_popup = get_field('use_welcome_popup', 'option');  ?>
